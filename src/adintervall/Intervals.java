97628ff450e376f9e28599eb4744c1e118f63f47
Bjoern Eberhardt <lazer.erazer@gmail.com>
2012-04-13 17:02:13 +0200

    saved unsaved changes

--- src/adintervall/Intervals.java
+++ src/adintervall/Intervals.java
@@ -2,4 +2,16 @@ package adintervall;
 
 public interface Intervals extends Interval {
 
+	// As Intervals is an Interval, and we might return an Interval, always use Interval as return value
+	// As Intervals is an Interval, and functions might be given an Intervals as an Interval, and an explicit
+	// supertype cast might be given, we probably need to extend the supertype function instead.
+	
+    //Interval plus(Intervals other);
+
+    //Interval minus(Intervals other);
+
+    //Interval multi(Intervals other);
+
+    //Interval div(Intervals other);
+
 }
