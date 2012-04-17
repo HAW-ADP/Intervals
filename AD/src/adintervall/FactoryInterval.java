2fe7ebba238fa068c255f059691933f44bcb43b0
1 <1@1-PC>
2012-04-17 18:56:20 +0200

    SchoencheitsAktion, alles unnoetiges ist geloescht

---
+++ AD/src/adintervall/FactoryInterval.java
@@ -0,0 +1,86 @@
+package adintervall;
+
+import java.util.*;
+
+public final class FactoryInterval {
+
+    private FactoryInterval() {
+    }
+
+    public static Interval createInterval(double val1, double val2) {
+        if (NormalInterval.isNaN(val1) || NormalInterval.isNaN(val2) || val1 > val2) {
+            return Interval.NaI;
+        } else if (val1 == 0d && val2 == 0d) {
+            return Interval.zeroInterval;
+        } else if (val1 == 1d && val2 == 1d) {
+            return Interval.oneInterval;
+        } else if (val1 == Interval.NEGATIVE_INFINITY && val2 == Interval.POSITIVE_INFINITY) {
+            return Interval.realInterval;
+        } else {
+            return new NormalInterval(val1, val2);
+        }
+    }
+
+    public static Interval createInterval(Double val) {
+        return createInterval(val, val);
+    }
+
+    public static Interval createInterval(Object... col) {
+        return createInterval(Arrays.asList(col));
+    }
+
+    public static Interval createInterval(Collection<? extends Object> col) {
+        LinkedList<Interval> lList = new LinkedList<>();
+        if (col.size() == 0) {
+            return Interval.emptyInterval;
+        } else {
+            for (Object obj : col) {
+                if (obj instanceof Double) {
+                    lList.add(createInterval((Double) obj));
+                } else if (obj instanceof Intervals) {
+                    lList.addAll(((Intervals) obj).getIntervals());
+                } else if (obj instanceof Interval && !(obj == Interval.NaI)) {
+                    lList.add((Interval) obj);
+                } else {
+                    return Interval.NaI;
+                }
+            }
+
+            Collections.sort(lList, new Comparator<Interval>() {
+
+                @Override
+                public int compare(Interval arg0, Interval arg1) {
+                    return Double.compare(arg0.getLowerBound(), arg1.getLowerBound());
+                }
+            });
+
+            LinkedList<Interval> result = new LinkedList<>();
+            Double lbound = null;
+            Double ubound = null;
+
+            for (Interval i : lList) {
+                if (i == Interval.realInterval) {
+                    return i;
+                } else if (ubound == null || ubound < i.getLowerBound()) {
+                    if (ubound != null) {
+                        result.push(new NormalInterval(lbound, ubound));
+                    }
+                    lbound = i.getLowerBound();
+                    ubound = i.getUpperBound();
+                } else if (ubound < i.getUpperBound()) {
+                    ubound = i.getUpperBound();
+                }
+            }
+            if (ubound != null) {
+                result.push(new NormalInterval(lbound, ubound));
+            }
+            if (result.size() == 0) {
+                return Interval.emptyInterval;
+            } else if (result.size() == 1) {
+                return result.get(0);
+            } else {
+                return new MultiIntervals(result);
+            }
+        }
+    }
+}
