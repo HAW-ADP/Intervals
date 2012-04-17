9a0ae82490f7ecacb0dbce75ad49f069a76d26e1
D. Fleischhauer <D.Fleischhauer@gmx.de>
2012-04-17 19:11:42 +0200

    test

--- src/adintervall/Intervals.java
+++ src/adintervall/Intervals.java
@@ -9,17 +9,4 @@ public interface Intervals extends Interval, Iterable<Interval> {
 	// As Intervals is an Interval, and we might return an Interval, always use Interval as return value
 	// As Intervals is an Interval, and functions might be given an Intervals as an Interval, and an explicit
 	// supertype cast might be given, we probably need to extend the supertype function instead.
-	
-    Interval plus(Intervals other);
-
-    Interval minus(Intervals other);
-
-    Interval multi(Intervals other);
-    
-    Interval plusKom(Interval other);
-    
-    Interval minusKom(Interval other);
-       
-    Interval multiKom(Interval other);
-
 }
