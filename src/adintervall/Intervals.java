f221e73b6ca3138cf39b99dbfc429eda585171de
Daniela Barths <daniela.barths@googlemail.com>
2012-06-02 22:59:22 +0200

    nichts geaendert

--- src/adintervall/Intervals.java
+++ src/adintervall/Intervals.java
@@ -4,8 +4,10 @@ public interface Intervals extends Interval, Iterable<Interval> {
 
 	int size();
 
+	
 	//public Collection<? extends Interval> getIntervals();
 
+	
 	// As Intervals is an Interval, and we might return an Interval, always use Interval as return value
 	// As Intervals is an Interval, and functions might be given an Intervals as an Interval, and an explicit
 	// supertype cast might be given, we probably need to extend the supertype function instead.
